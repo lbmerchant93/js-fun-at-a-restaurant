78a91c6286fd4361ad08dec45645eb7e6e4a11b8
Lucas Merchant <lbmerchant93@gmail.com>
2020-10-11 16:59:12 -0600

    Add functions and code to pass Chef-test.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,32 @@
 class Chef {
-  constructor() {
-    
-  }
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+    this.greetCustomer = function greeting(name, morningTime) {
+      if(morningTime === true) {
+        return `Good morning, ${name}!`;
+      };
+      return `Hello, ${name}!`;
+    };
+    this.checkForFood = function foodConfirmation(foodItem) {
+  for (var i = 0; i < restaurant.menus.breakfast.length; i++) {
+      if(restaurant.menus.breakfast.includes(foodItem)) {
+              return `Yes, we're serving ${foodItem.name} today!`;
+      };
+    };
+    for (var i = 0; i < restaurant.menus.lunch.length; i++) {
+        if(restaurant.menus.lunch.includes(foodItem)) {
+                return `Yes, we're serving ${foodItem.name} today!`;
+        };
+    };
+    for (var i = 0; i < restaurant.menus.dinner.length; i++) {
+        if(restaurant.menus.dinner.includes(foodItem)) {
+                return `Yes, we're serving ${foodItem.name} today!`;
+        };
+  };
+  return `Sorry, we aren't serving ${foodItem.name} today.`;
+};
+};
 }
 
 module.exports = Chef;
